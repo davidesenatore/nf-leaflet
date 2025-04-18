e7c3f6f45eb1d1417414ac855c07688143f43737
Davide Senatore <info@ingeniumsoft.com>
2025-04-18 13:44:43 +0200

    Working example with rollup

--- projects/mfe1/src/app/app.component.ts
+++ projects/mfe1/src/app/app.component.ts
@@ -1,46 +1,54 @@
-import { AfterViewInit, Component } from '@angular/core';
-// import { latLng, tileLayer } from 'leaflet';
-// import { LeafletModule } from '@bluehalo/ngx-leaflet';
-import lft from 'esm-leaflet';
+import { AfterViewInit, Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+import lft from 'leaflet-rep';
 
 @Component({
-  selector: 'app-root',  
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None   // Molto importante per i CSS
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements  AfterViewInit {
   title = 'mfe1';
-  
-    private map!: any;
-  
-    private initMap(): void {
-      this.map = lft.map('map', {
-        center: [45.4342, 12.3398], // Coordinate di Venezia
-        zoom: 15
-      });
-  
-      lft.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; OpenStreetMap contributors'
-      }).addTo(this.map);
-  
-      // Marker in Piazza San Marco
-      const sanMarcoMarker = lft.marker([45.4336, 12.3408]) // Piazza San Marco
-        .addTo(this.map)
-        .bindPopup('Piazza San Marco')
-        .openPopup();
-    }
-  
-    ngAfterViewInit(): void {
-      this.initMap();
-    }
 
-// ngOnInit() {
-//   const map = lfl.map('map').setView([45.4238816,12.3350456], 13);
+  private map!: any;
 
-//   lfl.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '© OpenStreetMap contributors'
-//   }).addTo(map);
-// }
+  private initMap(): void {
+    
+    //Imposto le icone esplicitamente impostando dove si trovano rispetto al MFE
+    lft.Icon.Default.mergeOptions(
+      {
+        iconUrl: 'http://localhost:4500/assets/leaflet/marker-icon.png',
+        shadowUrl: 'http://localhost:4500/assets/leaflet/marker-shadow.png',
+      }
+    );
+    this.map = lft.map('map', {
+      center: [45.4342, 12.3398], // Coordinate di Venezia
+      zoom: 15
+    });
+
+    lft.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    // Marker in Piazza San Marco
+    const sanMarcoMarker = lft.marker([45.4336, 12.3408]) // Piazza San Marco
+      .addTo(this.map)
+      .bindPopup('Piazza San Marco')
+      .openPopup();
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+  // ngOnInit() {
+  //   const map = lfl.map('map').setView([45.4238816,12.3350456], 13);
+
+  //   lfl.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  //     attribution: '© OpenStreetMap contributors'
+  //   }).addTo(map);
+  // }
 
 
 }
